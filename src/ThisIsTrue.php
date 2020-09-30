5260e319edb45818106e4d3d1ced326e157fd90d
Thiago Cordeiro <thiagoguetten@gmail.com>
2020-09-30 21:40:47 +0200

    Fixed mutation config

---
+++ src/ThisIsTrue.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class ThisIsTrue
+{
+    public function returnTrue(): bool
+    {
+        return true;
+    }
+}
--- tests/TrueIsTrueTest.php
+++ tests/TrueIsTrueTest.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Test;
 
+use App\ThisIsTrue;
 use PHPUnit\Framework\TestCase;
 
 class TrueIsTrueTest extends TestCase
 {
     public function testTrue(): void
     {
-        $this->assertTrue(true);
+        $sunshine = new ThisIsTrue();
+
+        $this->assertTrue($sunshine->returnTrue());
     }
 }
